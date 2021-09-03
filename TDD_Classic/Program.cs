27459bb7e9d85884cd0258f3032f8e090c2db88f
z004chyx <julia.gablikova@siemens-healthineers.com>
2021-09-03 13:36:44 +0200

    Initial commit

---
+++ TDD_Classic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD_Classic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
